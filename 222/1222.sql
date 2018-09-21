95fb1f9f1f7555b26a3511770ee95996000a7566
whx159 <1835298762@qqq.com>
2018-09-21 20:20:15 +0800

    add

---
+++ 222/1222.sql
@@ -0,0 +1,121 @@
+﻿prompt Importing table bank_mapping...
+set feedback off
+set define off
+insert into bank_mapping (BANK_ID, PMPAYC, PMPYTN, PMPYEN, PMPYAD, PMPTEL, PMPFAX, PMPEML)
+values ('BOT', '001', 'ธนาคารแห่งประเทศไทย', 'BANK OF THAILAND', '273 ถนนสามเสน บางขุนพรหม กรุงเทพฯ 10200', '0-2283-5353', '0-2280-0449', null);
+
+insert into bank_mapping (BANK_ID, PMPAYC, PMPYTN, PMPYEN, PMPYAD, PMPTEL, PMPFAX, PMPEML)
+values ('BBL', '002', 'ธนาคารกรุงเทพ จำกัด (มหาชน)', 'BANGKOK BANK PUBLIC COMPANY LIMITED (BBL)', '333 ถนนสีลม แขวงสุริยวงศ์ เขตบางรัก กรุงเทพฯ 10500', '0-2231-4333', '0-2236-8281-2', null);
+
+insert into bank_mapping (BANK_ID, PMPAYC, PMPYTN, PMPYEN, PMPYAD, PMPTEL, PMPFAX, PMPEML)
+values ('KBANK', '004', 'ธนาคารกสิกรไทย จำกัด (มหาชน)', 'KASIKORNBANK PUBLIC COMPANY LIMITED (KBANK)', '1 ซอยกสิกรไทย ถ.ราษฎร์บูรณะ แขวงราษฎร์บูรณะ เขตราษฎร์บูรณะ กรุงเทพฯ 10140', '0-2470-1122', '0-2470-1144', null);
+
+insert into bank_mapping (BANK_ID, PMPAYC, PMPYTN, PMPYEN, PMPYAD, PMPTEL, PMPFAX, PMPEML)
+values ('RBS', '005', 'ธนาคารเดอะรอยัลแบงก์อ๊อฟสกอตแลนด์ เอ็น.วี. สาขากรุงเทพฯ', 'THE ROYAL BANK OF SCOTLAND N.V. (RBS) , BANGKOK BRANCH', 'ชั้น 1-4 อาคารบางกอกซิตี้ทาวเวอร์ 179/3 ถนนสาทรใต้ แขวงทุ่งมหาเมฆ เขตสาธร กรุงเทพฯ 10120', '0-2679-5900', null, null);
+
+insert into bank_mapping (BANK_ID, PMPAYC, PMPYTN, PMPYEN, PMPYAD, PMPTEL, PMPFAX, PMPEML)
+values ('KTB', '006', 'ธนาคารกรุงไทย จำกัด (มหาชน)', 'KRUNG THAI BANK PUBLIC COMPANY LIMITED (KTB)', '35 ถนนสุขุมวิท แขวงคลองเตย เขตคลองเตย กรุงเทพฯ 10110', '0-2255-2222', '0-2255-9380', null);
+
+insert into bank_mapping (BANK_ID, PMPAYC, PMPYTN, PMPYEN, PMPYAD, PMPTEL, PMPFAX, PMPEML)
+values ('JPMC', '008', 'ธนาคารเจพีมอร์แกน เชส สาขากรุงเทพฯ', 'JPMORGAN CHASE BANK, BANGKOK BRANCH (JPMC)', '20 ถนนสาทรเหนือ แขวงสีลม เขตบางรัก กรุงเทพฯ 10500', '0-2684-2000', '0-2684-2460', null);
+
+insert into bank_mapping (BANK_ID, PMPAYC, PMPYTN, PMPYEN, PMPYAD, PMPTEL, PMPFAX, PMPEML)
+values ('BTMU', '010', 'ธนาคารแห่งโตเกียว-มิตซูบิชิ ยูเอฟเจ สาขากรุงเทพฯ', 'THE BANK OF TOKYO-MITSUBISHI UFJ, LIMITED, BANGKOK BRANCH (BTMU)', '54 อาคารหะรินธร ถนนสาทรเหนือ แขวงสีลม เขตบางรัก กรุงเทพฯ 10500', '0-2266-3011-35', '0-2266-3055', null);
+
+insert into bank_mapping (BANK_ID, PMPAYC, PMPYTN, PMPYEN, PMPYAD, PMPTEL, PMPFAX, PMPEML)
+values ('TMB', '011', 'ธนาคารทหารไทย จำกัด (มหาชน)', 'TMB BANK PUBLIC COMPANY LIMITED (TMB)', '3000 ถนนพหลโยธิน แขวงจอมพล เขตจตุจักร กรุงเทพฯ 10900', '0-2299-1111', '0-2273-7121', null);
+
+insert into bank_mapping (BANK_ID, PMPAYC, PMPYTN, PMPYEN, PMPYAD, PMPTEL, PMPFAX, PMPEML)
+values ('SCB', '014', 'ธนาคารไทยพาณิชย์ จำกัด (มหาชน)', 'SIAM COMMERCIAL BANK PUBLIC COMPANY LIMITED (SCB)', '9 ถนนรัชดาภิเษก แขวงลาดยาว เขตจตุจักร กรุงเทพฯ 10900', '0-2544-5678', '0-2937-7454', null);
+
+insert into bank_mapping (BANK_ID, PMPAYC, PMPYTN, PMPYEN, PMPYAD, PMPTEL, PMPFAX, PMPEML)
+values ('SCIB', '015', 'ธนาคารนครหลวงไทย จำกัด (มหาชน)', 'SIAM CITY BANK PUBLIC COMPANY LIMITED (SCIB)', '1101 ถนนเพชรบุรีตัดใหม่ แขวงมักกะสัน เขตราชเทวี กรุงเทพฯ 10320', '0-2253-0200', '0-2253-6331', null);
+
+insert into bank_mapping (BANK_ID, PMPAYC, PMPYTN, PMPYEN, PMPYAD, PMPTEL, PMPFAX, PMPEML)
+values ('CITI', '017', 'ธนาคารซิตี้แบงก์', 'CITIBANK, N.A. (CITI), BANGKOK BRANCH', '399 ถนนสุขุมวิท อาคารอินเตอร์เชนจ์ 21 แขวงคลองเตยเหนือ เขตวัฒนา กรุงเทพฯ 10110', '0-2788-2000', null, null);
+
+insert into bank_mapping (BANK_ID, PMPAYC, PMPYTN, PMPYEN, PMPYAD, PMPTEL, PMPFAX, PMPEML)
+values ('SMBC', '018', 'ธนาคารชูมิโตโม มิตซุย แบงกิ้ง คอร์ปอเรชั่น', 'SUMITOMO MITSUI BANKING CORPORATION (SMBC)', '1 อาคารคิวเฮาส์ ลุมพินี ชั้น 8-10 ถนนสาทรใต้ แขวงทุ่งมหาเมฆ เขตสาทร กรุงเทพฯ 10120', '0-2353-8000', '0-2353-8282', null);
+
+insert into bank_mapping (BANK_ID, PMPAYC, PMPYTN, PMPYEN, PMPYAD, PMPTEL, PMPFAX, PMPEML)
+values ('SCBT', '020', 'ธนาคารสแตนดาร์ดชาร์เตอร์ด (ไทย) จำกัด (มหาชน)', 'STANDARD CHARTERED BANK (THAI) PUBLIC COMPANY LIMITED (SCBT)', '90 ถนนสาทรเหนือ แขวงสีลม เขตบางรัก กรุงเทพฯ 10500', '0-2233-2111', '0-2236-1968', null);
+
+insert into bank_mapping (BANK_ID, PMPAYC, PMPYTN, PMPYEN, PMPYAD, PMPTEL, PMPFAX, PMPEML)
+values ('CIMBT', '022', 'ธนาคาร ซีไอเอ็มบี ไทย จำกัด (มหาชน)', 'CIMB THAI BANK PUBLIC COMPANY LIMITED (CIMBT)', 'สำนักงานใหญ่ 44 ถนนหลังสวน แขวงลุมพินี เขตปทุมวัน กรุงเทพฯ 10330', '0-2636-8000', '0-2657-3333', null);
+
+insert into bank_mapping (BANK_ID, PMPAYC, PMPYTN, PMPYEN, PMPYAD, PMPTEL, PMPFAX, PMPEML)
+values ('UOBT', '024', 'ธนาคารยูโอบี จำกัด (มหาชน)', 'UNITED OVERSEAS BANK (THAI) PUBLIC COMPANY LIMITED (UOBT)', '191 ถนนสาทรใต้ เขตสาทร กรุงเทพฯ 10120', '0-2343-3000', null, null);
+
+insert into bank_mapping (BANK_ID, PMPAYC, PMPYTN, PMPYEN, PMPYAD, PMPTEL, PMPFAX, PMPEML)
+values ('BAY', '025', 'ธนาคารกรุงศรีอยุธยา จำกัด (มหาชน)', 'BANK OF AYUDHYA PUBLIC COMPANY LIMITED (BAY)', '0-2296-2000', '0-2683-1484', null, null);
+
+insert into bank_mapping (BANK_ID, PMPAYC, PMPYTN, PMPYEN, PMPYAD, PMPTEL, PMPFAX, PMPEML)
+values ('MEGA ICBC', '026', 'ธนาคาร เมกะ สากลพาณิชย์ จำกัด (มหาชน)', 'MEGA INTERNATIONAL COMMERCIAL BANK PUBLIC COMPANY LIMITED (MEGA ICBC)', 'อาคาร พี.เอส. ทาวเวอร์ 36/12 ถนนสุขุมวิท ซอย 21 แขวงคลองตัน เขตวัฒนา กรุงเทพฯ 10400', '0-2259-2000-9', null, null);
+
+insert into bank_mapping (BANK_ID, PMPAYC, PMPYTN, PMPYEN, PMPYAD, PMPTEL, PMPFAX, PMPEML)
+values ('AMERICA', '027', 'ธนาคารแห่งอเมริกา เนชั่นแนล แอสโซซิเอชั่น', 'BANK OF AMERICA NATIONAL ASSOCIATION (AMERICA)', '87/2 ออลซีซั่นเพลส ซีอาร์ซี ทาวเวอร์ ชั้น 33 ถนนวิทยุ เขตปทุมวัน กรุงเทพฯ 10330', ' 0-2305-2900', '0-2305-2999', null);
+
+insert into bank_mapping (BANK_ID, PMPAYC, PMPYTN, PMPYEN, PMPYAD, PMPTEL, PMPFAX, PMPEML)
+values ('CACIB', '028', 'ธนาคารเครดิต อะกริกอล คอร์ปอเรทแอนด์อินเวสเมนท์แบงก์ สาขากรุงเทพฯ', 'CREDIT AGRICOLE CORPORATE AND INVESTMENT BANK (CACIB) BANGKOK BRANCH', '152 ถนนวิทยุ แขวงลุมพินี เขตปทุมวัน กรุงเทพฯ 10330', '0-2624-8000', '0-2651-4574', null);
+
+insert into bank_mapping (BANK_ID, PMPAYC, PMPYTN, PMPYEN, PMPYAD, PMPTEL, PMPFAX, PMPEML)
+values ('GSB', '030', 'ธนาคารออมสิน', 'GOVERNMENT SAVINGS BANK (GSB)', '470 ถนนพหลโยธิน แขวงสามเสนใน เขตพญาไท กรุงเทพฯ 10400', '0-2299-8000', null, null);
+
+insert into bank_mapping (BANK_ID, PMPAYC, PMPYTN, PMPYEN, PMPYAD, PMPTEL, PMPFAX, PMPEML)
+values ('HSBC', '031', 'ธนาคารฮ่องกงและเซี่ยงไฮ้แบงกิ้งคอร์ปอเรชั่น จำกัด', 'THE HONGKONG AND SHANGHAI BANKING CORPORATION LIMITED (HSBC)', '968 ถนนพระราม 4 แขวงสีลม เขตบางรัก กรุงเทพฯ', 'โทร. 0-2614-4000', null, null);
+
+insert into bank_mapping (BANK_ID, PMPAYC, PMPYTN, PMPYEN, PMPYAD, PMPTEL, PMPFAX, PMPEML)
+values ('DB', '32', 'ธนาคารดอยซ์แบงก์', 'DEUTSCHE BANK AKTIENGESELLSCHAFT (DB)', '208 ถนนวิทยุ แขวงลุมพินี เขตปทุมวัน กรุงเทพฯ 10330', '0-2651-5000', null, null);
+
+insert into bank_mapping (BANK_ID, PMPAYC, PMPYTN, PMPYEN, PMPYAD, PMPTEL, PMPFAX, PMPEML)
+values ('SHB', '033', 'ธนาคารอาคารสงเคราะห์', 'GOVERNMENT HOUSING BANK (GHB)', '212 ถนนพระราม 9 แขวงห้วยขวาง เขตห้วยขวาง กรุงเทพฯ 10320', '0-2246-0303', '0-2246-1789', null);
+
+insert into bank_mapping (BANK_ID, PMPAYC, PMPYTN, PMPYEN, PMPYAD, PMPTEL, PMPFAX, PMPEML)
+values ('BAAC', '034', 'ธนาคารเพื่อการเกษตรและสหกรณ์การเกษตร', 'BANK FOR AGRICULTURE AND AGRICULTURAL COOPERATIVES (BAAC)', '469 ถนนนครสวรรค์ แขวงจิตรลดา เขตดุสิต กรุงเทพฯ 10300', '0-2280-0180', null, null);
+
+insert into bank_mapping (BANK_ID, PMPAYC, PMPYTN, PMPYEN, PMPYAD, PMPTEL, PMPFAX, PMPEML)
+values ('MHCB', '039', 'ธนาคารมิซูโฮ คอร์ปอเรต จำกัด', 'MIZUHO CORPORATE BANK LIMITED (MHCB)', '48 อาคารทิสโก้ ชั้น 18 ถนนสาทรเหนือ แขวงสีลม เขตบางรัก กรุงเทพฯ 10500', ' 0-2638-0200-5', '0-2638-0218', null);
+
+insert into bank_mapping (BANK_ID, PMPAYC, PMPYTN, PMPYEN, PMPYAD, PMPTEL, PMPFAX, PMPEML)
+values ('BNPP', '045', 'ธนาคารบีเอ็นพี พารีบาส์ สาขากรุงเทพฯ', 'BNP PARIBAS, BANGKOK BRANCH (BNPP)', '990 อาคารอับดุลราฮิม เพลส ชั้น 29 ถนนพระรามที่ 4 แขวงสีลม เขตบางรัก กรุงเทพฯ 10500', '0-2636-1900', '0-2636-1935', null);
+
+insert into bank_mapping (BANK_ID, PMPAYC, PMPYTN, PMPYEN, PMPYAD, PMPTEL, PMPFAX, PMPEML)
+values ('TBANK', '065', 'ธนาคารธนชาต จำกัด (มหาชน)', 'THANACHART BANK PUBLIC COMPANY LIMITED (TBANK)', '900 อาคารต้นสน ทาวเวอร์ ถนนเพลินจิต แขวงลุมพินี เขตปทุมวัน กรุงเทพฯ 10330', '0-2655-9152-3', '0-2655-9181', null);
+
+insert into bank_mapping (BANK_ID, PMPAYC, PMPYTN, PMPYEN, PMPYAD, PMPTEL, PMPFAX, PMPEML)
+values ('ISBT', '066', 'ธนาคารอิสลามแห่งประเทศไทย', 'ISLAMIC BANK OF THAILAND (ISBT)', '66 อาคารคิวเฮ้าส์ อโศก ชั้น M, 21-22 ถนนสุขุมวิท 21 แขวงคลองเตยเหนือ เขตวัฒนา กรุงเทพฯ 10110', '02-204-2763', '0-2204-1556', null);
+
+insert into bank_mapping (BANK_ID, PMPAYC, PMPYTN, PMPYEN, PMPYAD, PMPTEL, PMPFAX, PMPEML)
+values ('TISCO', '067', 'ธนาคารทิสโก้ จำกัด (มหาชน)', 'TISCO BANK PUBLIC COMPANY LIMITED (TISCO)', '48/2 อาคารทิสโก้ทาวเวอร์ ถนนสาทรเหนือ แขวงสีลม เขตบางรัก กรุงเทพฯ 10500', '0-2633-6000', '0-2633-6800', null);
+
+insert into bank_mapping (BANK_ID, PMPAYC, PMPYTN, PMPYEN, PMPYAD, PMPTEL, PMPFAX, PMPEML)
+values ('KK', '069', 'ธนาคารเกียรตินาคิน จำกัด (มหาชน)', 'KIATNAKIN BANK PUBLIC COMPANY LIMITED (KK)', '500 อาคารอัมรินทร์ทาวเวอร์ ถนนเพลินจิต เขตปทุมวัน กรุงเทพฯ 10330', '0-2680-3668', '0-2257-0849', null);
+
+insert into bank_mapping (BANK_ID, PMPAYC, PMPYTN, PMPYEN, PMPYAD, PMPTEL, PMPFAX, PMPEML)
+values ('ICBC', '070', 'ธนาคารไอซีบีซี (ไทย) จำกัด (มหาชน)', 'INDUSTRIAL AND COMMERCIAL BANK OF CHINA (THAI) PUBLIC COMPANY LIMITED (ICBC (Thai))', '622 เอ็มโพเรี่ยม ทาวเวอร์ ชั้น 11-13 ถนนสุขุมวิท แขวงคลองตัน เขตคลองเตย กรุงเทพฯ 10110', '0-2663-9999', '0-2663-9888', null);
+
+insert into bank_mapping (BANK_ID, PMPAYC, PMPYTN, PMPYEN, PMPYAD, PMPTEL, PMPFAX, PMPEML)
+values ('TCRB', '071', 'ธนาคารไทยเครดิต เพื่อรายย่อย จำกัด (มหาชน)', 'THAI CREDIT RETAIL BANK PUBLIC COMPANY LIMITED (TCRB)', '123 อาคารไทยประกันชีวิต ถนนรัชดาภิเษก แขวงดินแดง เขตดินแดง กรุงเทพฯ 10400', '0-2697-5454', '0-2642-3122', null);
+
+insert into bank_mapping (BANK_ID, PMPAYC, PMPYTN, PMPYEN, PMPYAD, PMPTEL, PMPFAX, PMPEML)
+values ('LHB', '073', 'ธนาคารแลนด์ แอนด์ เฮ้าส์ เพื่อรายย่อย จำกัด (มหาชน)', 'LAND AND HOUSES RETAIL BANK PUBLIC COMPANY LIMITED (LH BANK)', '1 อาคารคิวเฮ้าส์ ลุมพินี ชั้น G, 1, 5 และ 6 ถนนสาทรใต้ แขวงทุ่งมหาเมฆ เขตสาทร กรุงเทพฯ 10120', '0-2677-7111', '0-2677-7208', null);
+
+insert into bank_mapping (BANK_ID, PMPAYC, PMPYTN, PMPYEN, PMPYAD, PMPTEL, PMPFAX, PMPEML)
+values ('7ELEVEN', '999', null, '7 Eleven Counter Service', null, 'TBA', 'TBA', null);
+
+insert into bank_mapping (BANK_ID, PMPAYC, PMPYTN, PMPYEN, PMPYAD, PMPTEL, PMPFAX, PMPEML)
+values ('TRUEMONEY', '999', null, 'True Money Shop', null, 'TBA', 'TBA', null);
+
+insert into bank_mapping (BANK_ID, PMPAYC, PMPYTN, PMPYEN, PMPYAD, PMPTEL, PMPFAX, PMPEML)
+values ('TESCO', '999', null, 'Tesco Lotus Counter Service', null, 'TBA', 'TBA', null);
+
+insert into bank_mapping (BANK_ID, PMPAYC, PMPYTN, PMPYEN, PMPYAD, PMPTEL, PMPFAX, PMPEML)
+values ('TOT', '999', null, 'Just Pay by TOT public Company Ltd.', null, 'TBA', 'TBA', null);
+
+insert into bank_mapping (BANK_ID, PMPAYC, PMPYTN, PMPYEN, PMPYAD, PMPTEL, PMPFAX, PMPEML)
+values ('PAP', '999', null, 'Pay at post', null, 'TBA', 'TBA', null);
+
+insert into bank_mapping (BANK_ID, PMPAYC, PMPYTN, PMPYEN, PMPYAD, PMPTEL, PMPFAX, PMPEML)
+values ('BIGC', '999', null, 'BigC', null, null, null, null);
+
+prompt Done.
