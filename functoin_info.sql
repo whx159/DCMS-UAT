67ad621f974a3cad70cfab623773a7ae8d329831
whx159 <1835298762@qqq.com>
2019-01-25 16:47:35 +0800

    add

--- functoin_info.sql
+++
@@ -1,7 +0,0 @@
-prompt Importing table function_info...
-set feedback off
-set define off
-insert into function_info (FUNCID, FUNCNAME, PAGEPATH, LOCATION, ISDIRECTORY, LASTDIRECTORY, SHOWSEQ, FUNC_CLASS, FUNC_TYPE, WORKFLOW_FLAG, UP_FUNC_CODE, FUNC_DESC, STATUS, EFFECT_DATE, EXPIRE_DATE, TIMESTAMPS, MISCFLGS, ICON_CLS, MISC)
-values ('3015', 'Static QR Code', '/fpages/hsbc/ftl/StaticQrCode.ftl', 2.00, 0.00, '30', 15.00, '1', '1', null, null, 'OTHER', '1', null, null, null, null, 'icon-edit', null);
-
-prompt Done.
