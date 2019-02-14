d09aaa0218b9847f7fee616fa516a937ce3f3736
whx159 <1835298762@qqq.com>
2019-02-14 10:12:20 +0800

    add

---
+++ whx/12311.sql
@@ -0,0 +1,7 @@
+prompt Importing table sys_params...
+set feedback off
+set define off
+insert into sys_params (PARAMGROUP_ID, PARAM_ID, PARAM_NAME, PARAM_VAL, MEMO, ST, IS_LOCK, IS_DEL, CRT_DT, LAST_UPD_TMS, LAST_UPD_OPER, SYS_NAME)
+values ('QRIMAGE', 'QRPATH', 'Í¼Æ¬Â·¾¶', 'G:/qrImage/', 'Í¼Æ¬Â·¾¶', '4', 'F', 'F', '20171017', '20171017153321', 'dlzl', 'AML');
+
+prompt Done.
